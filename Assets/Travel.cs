752c46d267d58b7c525928e4de74a3c03f6c0648
KieranG32 <16016629@students.southwales.ac.uk>
2020-10-28 20:33:27 +0000

    Level transition added
    There is a trigger in the folliage , when stood in the level will transition to the selected level in the Travel script, to change the destination you change the line
    
    SceneManager.LoadScene(0);
    To:
    SceneManager.LoadScene(1);
    
    The number at the end correlates to the scene's number/order in the build menu

---
+++ Assets/Travel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Travel : MonoBehaviour
+{
+    void OnTriggerEnter (Collider other)
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+}
