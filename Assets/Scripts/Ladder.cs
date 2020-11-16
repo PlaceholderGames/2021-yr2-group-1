3eda4c968e504477426a7365e3db88d2b1dabad4
KieranG32 <16016629@students.southwales.ac.uk>
2020-11-16 20:23:30 +0000

    Climbing
    I have started the climbing mechanic but ran into a error i am yet to fix
    
    When the player leaves the trigger the player continues to rise on the Y axis (Up) but a while breaks the games runtime via a endless loop? and onTriggerExit doesnt seem to activate to stop the player from floating up

---
+++ Assets/Scripts/Ladder.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour
+{
+    public CharacterController playerController;
+    public GameObject Player;
+    private bool IsTrigger = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            playerController.enabled = false;
+            IsTrigger = true;
+        }
+        
+        
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Exit");
+            IsTrigger = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsTrigger == true)
+        {
+
+
+            Player.transform.Translate(0, 0.01f, 0, Space.World);
+
+        }
+        if (IsTrigger == false)
+        {
+            playerController.enabled = true;
+            Player.transform.Translate(0, 0, 0, Space.World);
+        }
+    }
+
+
+}
+
