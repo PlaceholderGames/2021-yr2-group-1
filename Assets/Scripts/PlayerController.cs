186d40a95a062518107f07a5a18a0c03b3940f04
Joseph <jwallen76543@gmail.com>
2020-11-20 17:12:42 +0000

    Moved and modified ladder script functionality into the playerController and added opening door object with script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,8 +12,12 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public float verticalVelocity;
     public CharacterController controller;
-    private Vector3 moveDirection;
+    public Vector3 moveDirection;
     public float gravityScale;
+
+    //Ladder Variables
+    public bool ladderCollision;
+    public bool ladderToggle;
 
     //animator variable
     public Animator anim;
@@ -27,61 +31,110 @@ public class PlayerController : MonoBehaviour
     {
         //Instantiates controller
         controller = GetComponent<CharacterController>();
-        torch.SetActive(false); 
+        torch.SetActive(false);
+    }
+
+    //Ladder Collision Checks
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ladder")
+        {
+            ladderCollision = true;
+        }
+    }
+    //Checks the conditions every frame the player is within the ladder collision box
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Ladder" && ladderToggle == false && Input.GetKeyDown(KeyCode.E))
+        {
+            ladderToggle = true;
+        }
+        else if (other.gameObject.tag == "Ladder" && ladderToggle == true && Input.GetKeyDown(KeyCode.E))
+        {
+            ladderToggle = false;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Ladder")
+        {
+            ladderCollision = false;
+            ladderToggle = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Allows for input of all cardinal directions and cominations of them
-        moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
-        //Stops speed being doubled on diagonal input
-        moveDirection = moveDirection.normalized * moveSpeed;
-        //Sets Y value to verticalVelocity
-        moveDirection.y = verticalVelocity;
-
-        //Checks if player is on the ground
-        if (controller.isGrounded)
+        if (ladderToggle == false)
         {
+            //Allows for input of all cardinal directions and cominations of them
+            moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+            //Stops speed being doubled on diagonal input
+            moveDirection = moveDirection.normalized * moveSpeed;
+            //Sets Y value to verticalVelocity
+            moveDirection.y = verticalVelocity;
+
+            //Checks if player is on the ground
+            if (controller.isGrounded)
+            {
+                //Checks if shift is held and changes speed
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    moveSpeed = sprintSpeed;
+                }
+                //Checks if shift is not held and reverts speed to default
+                else if (!Input.GetKey(KeyCode.LeftShift))
+                {
+                    moveSpeed = 5.0f;
+                }
+
+                //Checks if jump button is pressed and jumps if so
+                if (Input.GetButton("Jump"))
+                {
+                    verticalVelocity = jumpForce;
+                }
+                else if (!Input.GetButton("Jump"))
+                {
+                    verticalVelocity = 0f;
+                }
+            }
+
+            if (!controller.isGrounded)
+            {
+                //Gradually takes away gravity from verticalVelocity on jump
+                verticalVelocity -= gravityScale * Time.deltaTime;
+            }
+
+            //Checks if torchToggles is off and sets to true
+            if (torchToggle == false && Input.GetKeyDown(KeyCode.F))
+            {
+                torchToggle = true;
+                torch.SetActive(true);
+            }
+            //Checks if torchToggles is on and sets to false
+            else if (torchToggle == true && Input.GetKeyDown(KeyCode.F))
+            {
+                torchToggle = false;
+                torch.SetActive(false);
+            }
+        }
+        else if (ladderToggle == true)
+        {
+            //Allows for input of all cardinal directions and cominations of them
+            moveDirection = (transform.up * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+            //Stops speed being doubled on diagonal input
+            moveDirection = moveDirection.normalized * moveSpeed;
             //Checks if shift is held and changes speed
             if (Input.GetKey(KeyCode.LeftShift))
             {
-                moveSpeed = sprintSpeed;
+                moveSpeed = sprintSpeed/2;
             }
             //Checks if shift is not held and reverts speed to default
             else if (!Input.GetKey(KeyCode.LeftShift))
             {
                 moveSpeed = 5.0f;
             }
-
-            //Checks if jump button is pressed and jumps if so
-            if (Input.GetButton("Jump"))
-            {
-                verticalVelocity = jumpForce;
-            }
-            else if (!Input.GetButton("Jump"))
-            {
-                verticalVelocity = 0f;
-            }
-        }
-
-        if (!controller.isGrounded)
-        {
-            //Gradually takes away gravity from verticalVelocity on jump
-            verticalVelocity -= gravityScale * Time.deltaTime;
-        }
-
-        //Checks if torchToggles is off and sets to true
-        if (torchToggle == false && Input.GetKeyDown(KeyCode.F))
-        {
-            torchToggle = true;
-            torch.SetActive(true);
-        }
-        //Checks if torchToggles is on and sets to false
-        else if (torchToggle == true && Input.GetKeyDown(KeyCode.F))
-        {
-            torchToggle = false;
-            torch.SetActive(false);
         }
 
         //Sends input to controller for movement
