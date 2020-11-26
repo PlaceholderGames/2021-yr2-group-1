3faee9bc3b3c6de02b35075ff82c8f1af1037ada
Wgcsam <55789479+Wgcsam@users.noreply.github.com>
2020-11-26 13:23:09 +0000

    updated pathfinding with new script and waypoints to account for old bug

--- Assets/Character Models/NPC's/AIpatrol.cs
+++ Assets/Character Models/NPC's/AIpatrol.cs
@@ -9,9 +9,12 @@ using UnityEngine.AI;
 public class AIpatrol : MonoBehaviour
 {
 
-    public Transform[] points; // create an array of points which I can assign for the NPC to move between
-    private int destPoint = 0; // initialising the destination point
+    //public Transform[] points; // create an array of points which I can assign for the NPC to move between
+    //private int destPoint = 0; // initialising the destination point
     private NavMeshAgent agent;
+
+    [SerializeField]
+    Transform _destination;
 
     //animator variable
     public Animator anim;
@@ -21,6 +24,14 @@ public class AIpatrol : MonoBehaviour
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        if (agent == null)
+        {
+            Debug.LogError("the nav mesh is not attached to " + gameObject.name);
+        }
+        else
+        {
+            SetDestination();
+        }
 
         // Disabling auto-braking allows for continuous movement
         // between points (ie, the agent doesn't slow down as it
@@ -28,28 +39,36 @@ public class AIpatrol : MonoBehaviour
         //turning this off as i want it to happen for now
         // agent.autoBraking = false;
 
-        GotoNextPoint();
     }
 
-    void GotoNextPoint()
+    private void SetDestination()
     {
-        // Returns if no points have been set up
-        if (points.Length == 0)
-            return;
-
-        //something to sleep the ai in here 
-        isWalking = false;
-
-        // Set the agent to go to the currently selected destination.
-        agent.destination = points[destPoint].position;
-
-        //sets state of is walking to true
-        isWalking = true;
-
-        // Choose the next point in the array as the destination,
-        // cycling to the start if necessary.
-        destPoint = (destPoint + 1) % points.Length;
+        if(_destination != null)
+        {
+            Vector3 targetVector = _destination.transform.position;
+            agent.SetDestination(targetVector);
+        }
     }
+
+    //void GotoNextPoint()
+    //{
+    //    // Returns if no points have been set up
+    //    if (points.Length == 0)
+    //        return;
+
+    //    //something to sleep the ai in here 
+    //    isWalking = false;
+
+    //    // Set the agent to go to the currently selected destination.
+    //    agent.destination = points[destPoint].position;
+
+    //    //sets state of is walking to true
+    //    isWalking = true;
+
+    //    // Choose the next point in the array as the destination,
+    //    // cycling to the start if necessary.
+    //    destPoint = (destPoint + 1) % points.Length;
+    //}
 
 
     // Update is called once per frame
@@ -60,7 +79,7 @@ public class AIpatrol : MonoBehaviour
 
         // Choose the next destination point when the agent gets
         // close to the current one.
-        if (!agent.pathPending && agent.remainingDistance < 0.5f)
-            GotoNextPoint();
+        //if (!agent.pathPending && agent.remainingDistance < 0.0f)
+        //    GotoNextPoint();
     }
 }
---
+++ Assets/Scripts/AISimplePatrol.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AISimplePatrol : MonoBehaviour
+{
+    //Dictates whether the agent waits on each node.
+    [SerializeField]
+    bool patrolWaiting;
+
+    //the total time we wait at each node
+    [SerializeField]
+    float totalWaitTime = 3f;
+
+    //probability of switching direction
+    [SerializeField]
+    float switchProbability = 0.2f;
+
+    //list of nodes to visit
+    [SerializeField]
+    List<Waypoint> patrolPoints;
+
+    //variables for base behaviour
+    UnityEngine.AI.NavMeshAgent agent;
+    int currentPatrolIndex;
+    bool travelling;
+    bool waiting;
+    bool patrolForward;
+    float waitTimer;
+
+    //animator variables
+    public Animator anim;
+    public bool isWalking = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+        if (agent == null)
+        {
+            Debug.LogError("nav mesh component not attached to " + gameObject.name);
+        }
+        else
+        {
+            if (patrolPoints != null && patrolPoints.Count >= 2)
+            {
+                currentPatrolIndex = 0;
+                SetDestination();
+            }
+            else
+            {
+                Debug.Log("Insufficient patrol points for behaviour");
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check if close to destination
+        if (travelling && agent.remainingDistance <= 1.0f)
+        {
+            travelling = false;
+
+            //if wait, then wait
+            if (patrolWaiting)
+            {
+                waiting = true;
+                waitTimer = 0f;
+            }
+            else
+            {
+                ChangePatrolPoint();
+                SetDestination();
+            }
+        }
+
+        //instead if waiting
+        if (waiting)
+        {
+            isWalking = false;
+
+            waitTimer += Time.deltaTime;
+            if (waitTimer >= totalWaitTime)
+            {
+                waiting = false;
+                isWalking = true;
+
+                ChangePatrolPoint();
+                SetDestination();
+            }
+        }
+
+        //update animator for walking or not
+        anim.SetBool("walking", isWalking);
+    }
+
+    private void SetDestination()
+    {
+        if (patrolPoints != null)
+        {
+            isWalking = true;
+
+            Vector3 targetVector = patrolPoints[currentPatrolIndex].transform.position;
+            agent.SetDestination(targetVector);
+            travelling = true;
+        }
+    }
+
+    private void ChangePatrolPoint()
+    {
+        if (UnityEngine.Random.Range(0f, 1f) <= switchProbability)
+        {
+            patrolForward = !patrolForward;
+        }
+
+        if (patrolForward)
+        {
+            currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Count; //check if exceeded list and reset to 0 if so
+        }
+        else
+        {
+            if (--currentPatrolIndex < 0)
+            {
+                currentPatrolIndex = patrolPoints.Count - 1;
+            }
+        }
+    }
+}
