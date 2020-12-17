2da1d99835e52d019e4d6fac4d7b4a874f00921d
Wgcsam <55789479+Wgcsam@users.noreply.github.com>
2020-12-17 10:01:19 +0000

    fixed one error

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -46,6 +46,7 @@ public class GameControl : MonoBehaviour
         //UnityEngine.Debug.Log("Player Position: X = " + playerObj.transform.position.x + " --- Y = " + playerObj.transform.position.y + " --- Z = " + playerObj.transform.position.z); //debug no longer needed
         if (playerObj == null) playerObj = GameObject.Find("playerCharacter");
         if (playerController == null) playerController = playerObj.GetComponent<CharacterController>();
+        if (pauseMenu == null) pauseMenu = playerObj.GetComponent<PauseMenu>();
     }
 
     private void Start()
