ef900e148129fcc5bcdcf83bd7707e933e30f955
Wgcsam <55789479+Wgcsam@users.noreply.github.com>
2021-01-28 12:34:01 +0000

    updated rop script to incremently remove above the burn

--- Assets/Scripts/RopeScript.cs
+++ Assets/Scripts/RopeScript.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class RopeScript : MonoBehaviour
 {
     public int destroyTime = 1;
+    //public GameObject above;
+    //public GameObject below;
+    public GameObject[] allAbove;
 
     //Detect collisions between the GameObjects with Colliders attached
     void OnCollisionEnter(Collision collision)
@@ -13,6 +16,18 @@ public class RopeScript : MonoBehaviour
         if (collision.gameObject.name == "Torch")
         {
             Destroy(gameObject, destroyTime);
+            for (int i=0;i<allAbove.Length;i++) //cycle through the array and remove each item 1 second at a time
+            {
+                //add in material change
+                Destroy(allAbove[i], (destroyTime + i));
+            }
+            //if (above != null) Destroy(above, destroyTime);
+            //if (below != null) Destroy(below, destroyTime);
+            //Debug.Log(this.transform.parent.gameObject.name);
+            //Destroy(this.transform.parent.gameObject, (destroyTime*3));
         }
     }
 }
+
+//would be good to set up an array which it passes through recursively
+//BG_lava
