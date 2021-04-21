6fd8e059b98fecc5b8feb2b39253380475206f40
Wgcsam <55789479+Wgcsam@users.noreply.github.com>
2021-04-21 11:26:42 +0100

    updated sound fx for rock collection and added sfx for door moving on kieran level

--- Assets/Scripts/DestroyBlock.cs
+++ Assets/Scripts/DestroyBlock.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class DestroyBlock : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private AudioClip clip;
+    private GameObject player = null;
+
+    void Awake()
     {
-        
+        player = GameObject.Find("playerCharacter");
     }
 
     // Update is called once per frame
@@ -15,6 +18,7 @@ public class DestroyBlock : MonoBehaviour
     {
         if (GameControl.control.Rocks >= 4)
         {
+            AudioSource.PlayClipAtPoint(clip, player.transform.position, 0.3f);
             Destroy(this.gameObject);
         }
     }
