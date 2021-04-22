9c393662501fcf1f35622967fddcdbf60aaefb69
Wgcsam <55789479+Wgcsam@users.noreply.github.com>
2021-04-22 12:43:25 +0100

    added in different background musics for different levels

--- Assets/Scripts/AudioControl.cs
+++ Assets/Scripts/AudioControl.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine.Audio;
 using UnityEngine;
 using System;
+using UnityEngine.SceneManagement;
 
 public class AudioControl : MonoBehaviour
 {
@@ -8,6 +9,9 @@ public class AudioControl : MonoBehaviour
     public Sound[] sounds;
 
     public static AudioControl instance;
+
+    private Scene currentScene;
+    private string isPlaying;
 
     void Awake()
     {
@@ -32,11 +36,55 @@ public class AudioControl : MonoBehaviour
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
         }
+
+        currentScene = SceneManager.GetActiveScene();
     }
 
     void Start ()
     {
         Play("Theme");
+    }
+
+    void Update ()
+    {
+        if (currentScene != SceneManager.GetActiveScene())
+        {
+            currentScene = SceneManager.GetActiveScene();
+            switch (currentScene.name)
+            {
+                case "Camp.lvl":
+                    if (isPlaying != "Theme")
+                    {
+                        Stop(isPlaying);
+                        Play("Theme");
+                    }
+                    break;
+                case "K_Temple_Room 1":
+                    Stop(isPlaying);
+                    Play("Kieran");
+                    break;
+                case "DeansLvl":
+                    Stop(isPlaying);
+                    Play("Dean");
+                    break;
+                case "Templefinal":
+                    Stop(isPlaying);
+                    Play("Temple");
+                    break;
+                case "Dragon":
+                    Stop(isPlaying);
+                    Play("Joe");
+                    break;
+                case "Room3":
+                    Stop(isPlaying);
+                    Play("Sam");
+                    break;
+                case "Credits":
+                    Stop(isPlaying);
+                    Play("End");
+                    break;
+            }
+        }
     }
 
     public void Play (string name)
@@ -48,5 +96,17 @@ public class AudioControl : MonoBehaviour
             return;
         }
         s.source.Play();
+        isPlaying = name;
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Stop();
     }
 }
